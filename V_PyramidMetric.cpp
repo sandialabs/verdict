95f9abcf4318727bb736c0818410c1bd7cd108ae
Karl Merkley <kmerkle@sandia.gov>
2012-06-29 17:39:44 +0000

    Pyramid quality implementations.  Shape, volume, jacobian, scaled jacobian are implemented.  Need to test test these more.
    git-svn-id: file:///scratch/cjstimp/svn/cubit/CUBIT_SOURCE/trunk@383422 d353b8dc-a10e-11dd-9ca7-d543032e456e

--- V_PyramidMetric.cpp
+++ V_PyramidMetric.cpp
@@ -24,10 +24,15 @@
 
 #include "verdict.h"
 #include "VerdictVector.hpp"
+#include "verdict.h"
 #include <memory.h> 
 
 
-
+// local method
+void v_make_pyramid_tets(double coordinates[][3], double tet1_coords[][3], double tet2_coords[][3],
+                                                  double tet3_coords[][3], double tet4_coords[][3]);
+void v_make_pyramid_faces(double coordinates[][3], double base[][3], double tri1[][3],
+                          double tri2[][3], double tri3[][3], double tri4[][3]);
 /*
   the pyramid element
 
@@ -105,6 +110,252 @@ C_FUNC_DEF double v_pyramid_volume( int num_nodes, double coordinates[][3] )
     
 }
 
+C_FUNC_DEF double v_pyramid_jacobian( int num_nodes, double coordinates[][3] )
+{
+  // break the pyramid into four tets return the minimum scaled jacobian of the two tets
+  double tet1_coords[4][3];
+  double tet2_coords[4][3];
+  double tet3_coords[4][3];
+  double tet4_coords[4][3];
+
+  v_make_pyramid_tets(coordinates, tet1_coords, tet2_coords, tet3_coords, tet4_coords);
+
+  double j1 = v_tet_jacobian(4, tet1_coords);
+  double j2 = v_tet_jacobian(4, tet2_coords);
+  double j3 = v_tet_jacobian(4, tet3_coords);
+  double j4 = v_tet_jacobian(4, tet4_coords);
+  
+  double p1 = j1 + j2;
+  double p2 = j3 + j4;
+
+  return p1 < p2 ? p1 : p2;
+}
+
+C_FUNC_DEF double v_pyramid_scaled_jacobian( int num_nodes, double coordinates[][3] )
+{
+   // lets start with making the vectors
+  VerdictVector edges[8];
+  edges[0].set( coordinates[1][0] - coordinates[0][0],
+                coordinates[1][1] - coordinates[0][1],
+                coordinates[1][2] - coordinates[0][2] );
+
+  edges[1].set( coordinates[2][0] - coordinates[1][0],
+                coordinates[2][1] - coordinates[1][1],
+                coordinates[2][2] - coordinates[1][2] );
+
+  edges[2].set( coordinates[3][0] - coordinates[2][0],
+                coordinates[3][1] - coordinates[2][1],
+                coordinates[3][2] - coordinates[2][2] );
+
+  edges[3].set( coordinates[3][0] - coordinates[0][0],
+                coordinates[3][1] - coordinates[0][1],
+                coordinates[3][2] - coordinates[0][2] );
+
+  edges[4].set( coordinates[4][0] - coordinates[0][0],
+                coordinates[4][1] - coordinates[0][1],
+                coordinates[4][2] - coordinates[0][2] );
+
+  edges[5].set( coordinates[4][0] - coordinates[1][0],
+                coordinates[4][1] - coordinates[1][1],
+                coordinates[4][2] - coordinates[1][2] );
+
+  edges[6].set( coordinates[4][0] - coordinates[2][0],
+                coordinates[4][1] - coordinates[2][1],
+                coordinates[4][2] - coordinates[2][2] );
+
+  edges[7].set( coordinates[4][0] - coordinates[3][0],
+                coordinates[4][1] - coordinates[3][1],
+                coordinates[4][2] - coordinates[3][2] );
+
+  // break the pyramid into two tets return the minimum scaled jacobian of the two tets
+  double tet1_coords[4][3];
+  double tet2_coords[4][3];
+  double tet3_coords[4][3];
+  double tet4_coords[4][3];
+
+  v_make_pyramid_tets(coordinates, tet1_coords, tet2_coords, tet3_coords, tet4_coords);
+
+  double j1 = v_tet_jacobian(4, tet1_coords);
+  double j2 = v_tet_jacobian(4, tet2_coords);
+  double j3 = v_tet_jacobian(4, tet3_coords);
+  double j4 = v_tet_jacobian(4, tet4_coords);
+
+  double scale1 = sqrt(edges[0] % edges[0]) * sqrt(edges[1] % edges[1]) * sqrt(edges[5] % edges[5]);
+  double scale2 = sqrt(edges[2] % edges[2]) * sqrt(edges[3] % edges[3]) * sqrt(edges[7] % edges[7]);
+  double scale3 = sqrt(edges[0] % edges[0]) * sqrt(edges[3] % edges[3]) * sqrt(edges[4] % edges[4]);
+  double scale4 = sqrt(edges[1] % edges[1]) * sqrt(edges[2] % edges[2]) * sqrt(edges[6] % edges[6]);
+
+  const double factor = 1/sqrt(2.0);
+  double js1 = factor*( j1/scale1 + j2/scale2);
+  double js2 = factor*( j3/scale3 + j4/scale4);
+  return js1 < js2 ? js1 : js2;
+}
+
+C_FUNC_DEF double v_pyramid_shape( int num_nodes, double coordinates[][3] )
+{
+  // ideally there will be four equilateral triangles and one square.  
+  // Test each face
+  double base[4][3];
+  double tri1[3][3];
+  double tri2[3][3];
+  double tri3[3][3];
+  double tri4[3][3];
+
+  v_make_pyramid_faces(coordinates, base, tri1, tri2, tri3, tri4);
+
+  double s1 = v_quad_shape(4, base);
+  double s2 = v_tri_shape(3, tri1);
+  double s3 = v_tri_shape(3, tri2);
+  double s4 = v_tri_shape(3, tri3);
+  double s5 = v_tri_shape(3, tri4);
+
+  return .2*(s1 + s2 + s3 + s4 + s5);
+}
+
+void v_make_pyramid_tets(double coordinates[][3], double tet1_coords[][3], double tet2_coords[][3],
+                                                  double tet3_coords[][3], double tet4_coords[][3])
+{
+   // tet1
+  tet1_coords[0][0] = coordinates[0][0];
+  tet1_coords[0][1] = coordinates[0][1];
+  tet1_coords[0][2] = coordinates[0][2];
+
+  tet1_coords[1][0] = coordinates[1][0];
+  tet1_coords[1][1] = coordinates[1][1];
+  tet1_coords[1][2] = coordinates[1][2];
+
+  tet1_coords[2][0] = coordinates[2][0];
+  tet1_coords[2][1] = coordinates[2][1];
+  tet1_coords[2][2] = coordinates[2][2];
+
+  tet1_coords[3][0] = coordinates[4][0];
+  tet1_coords[3][1] = coordinates[4][1];
+  tet1_coords[3][2] = coordinates[4][2];
+
+  // tet2
+  tet2_coords[0][0] = coordinates[0][0];
+  tet2_coords[0][1] = coordinates[0][1];
+  tet2_coords[0][2] = coordinates[0][2];
+
+  tet2_coords[1][0] = coordinates[2][0];
+  tet2_coords[1][1] = coordinates[2][1];
+  tet2_coords[1][2] = coordinates[2][2];
+
+  tet2_coords[2][0] = coordinates[3][0];
+  tet2_coords[2][1] = coordinates[3][1];
+  tet2_coords[2][2] = coordinates[3][2];
+
+  tet2_coords[3][0] = coordinates[4][0];
+  tet2_coords[3][1] = coordinates[4][1];
+  tet2_coords[3][2] = coordinates[4][2];
+
+  // tet3
+  tet3_coords[0][0] = coordinates[1][0];
+  tet3_coords[0][1] = coordinates[1][1];
+  tet3_coords[0][2] = coordinates[1][2];
+
+  tet3_coords[1][0] = coordinates[2][0];
+  tet3_coords[1][1] = coordinates[2][1];
+  tet3_coords[1][2] = coordinates[2][2];
+
+  tet3_coords[2][0] = coordinates[0][0];
+  tet3_coords[2][1] = coordinates[0][1];
+  tet3_coords[2][2] = coordinates[0][2];
+
+  tet3_coords[3][0] = coordinates[4][0];
+  tet3_coords[3][1] = coordinates[4][1];
+  tet3_coords[3][2] = coordinates[4][2];
+
+  // tet4
+  tet4_coords[0][0] = coordinates[2][0];
+  tet4_coords[0][1] = coordinates[2][1];
+  tet4_coords[0][2] = coordinates[2][2];
+
+  tet4_coords[1][0] = coordinates[3][0];
+  tet4_coords[1][1] = coordinates[3][1];
+  tet4_coords[1][2] = coordinates[3][2];
+
+  tet4_coords[2][0] = coordinates[0][0];
+  tet4_coords[2][1] = coordinates[0][1];
+  tet4_coords[2][2] = coordinates[0][2];
+
+  tet4_coords[3][0] = coordinates[4][0];
+  tet4_coords[3][1] = coordinates[4][1];
+  tet4_coords[3][2] = coordinates[4][2];
+}
+
+void v_make_pyramid_faces(double coordinates[][3], double base[][3], double tri1[][3],
+                          double tri2[][3], double tri3[][3], double tri4[][3])
+{
+   // base
+  base[0][0] = coordinates[0][0];
+  base[0][1] = coordinates[0][1];
+  base[0][2] = coordinates[0][2];
+
+  base[1][0] = coordinates[1][0];
+  base[1][1] = coordinates[1][1];
+  base[1][2] = coordinates[1][2];
+
+  base[2][0] = coordinates[2][0];
+  base[2][1] = coordinates[2][1];
+  base[2][2] = coordinates[2][2];
+
+  base[3][0] = coordinates[3][0];
+  base[3][1] = coordinates[3][1];
+  base[3][2] = coordinates[3][2];
+
+  // tri1
+  tri1[0][0] = coordinates[0][0];
+  tri1[0][1] = coordinates[0][1];
+  tri1[0][2] = coordinates[0][2];
+
+  tri1[1][0] = coordinates[1][0];
+  tri1[1][1] = coordinates[1][1];
+  tri1[1][2] = coordinates[1][2];
+
+  tri1[2][0] = coordinates[4][0];
+  tri1[2][1] = coordinates[4][1];
+  tri1[2][2] = coordinates[4][2];
+
+  // tri2
+  tri2[0][0] = coordinates[1][0];
+  tri2[0][1] = coordinates[1][1];
+  tri2[0][2] = coordinates[1][2];
+
+  tri2[1][0] = coordinates[2][0];
+  tri2[1][1] = coordinates[2][1];
+  tri2[1][2] = coordinates[2][2];
+
+  tri2[2][0] = coordinates[4][0];
+  tri2[2][1] = coordinates[4][1];
+  tri2[2][2] = coordinates[4][2];
+
+  // tri3
+  tri3[0][0] = coordinates[2][0];
+  tri3[0][1] = coordinates[2][1];
+  tri3[0][2] = coordinates[2][2];
+
+  tri3[1][0] = coordinates[3][0];
+  tri3[1][1] = coordinates[3][1];
+  tri3[1][2] = coordinates[3][2];
+
+  tri3[2][0] = coordinates[4][0];
+  tri3[2][1] = coordinates[4][1];
+  tri3[2][2] = coordinates[4][2];
+
+   // tri4
+  tri4[0][0] = coordinates[3][0];
+  tri4[0][1] = coordinates[3][1];
+  tri4[0][2] = coordinates[3][2];
+
+  tri4[1][0] = coordinates[0][0];
+  tri4[1][1] = coordinates[0][1];
+  tri4[1][2] = coordinates[0][2];
+
+  tri4[2][0] = coordinates[4][0];
+  tri4[2][1] = coordinates[4][1];
+  tri4[2][2] = coordinates[4][2];
+}
 
 
 C_FUNC_DEF void v_pyramid_quality( int num_nodes, double coordinates[][3], 
@@ -114,5 +365,11 @@ C_FUNC_DEF void v_pyramid_quality( int num_nodes, double coordinates[][3],
 
   if(metrics_request_flag & V_PYRAMID_VOLUME)
     metric_vals->volume = v_pyramid_volume(num_nodes, coordinates);
+  else if(metrics_request_flag & V_PYRAMID_JACOBIAN)
+    metric_vals->jacobian = v_pyramid_jacobian(num_nodes, coordinates);
+  else if(metrics_request_flag & V_PYRAMID_SCALED_JACOBIAN)
+    metric_vals->scaled_jacobian = v_pyramid_scaled_jacobian(num_nodes, coordinates);
+  else if(metrics_request_flag & V_PYRAMID_SHAPE)
+    metric_vals->shape = v_pyramid_shape(num_nodes, coordinates);
 }
 
