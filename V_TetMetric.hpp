b86d2c2f1b980867556002a91b822076bc8d3761
Corey Ernst <cdernst@sandia.gov>
2017-08-04 02:41:00 +0000

    1. Changing 'dimension' metric in hex quality to 'characteristic_length'. 2. Adding 'characteristic_length' metric to tet metrics. 'dimension' (for hexes) will still work but a warning is output telling the user to use 'characteristic_length' instead. 3. Changed enum CHARDIM to CHARACTERISTIC_LENGTH. 4. Refactored tet_dimension function in V_TetMetric to use VerdictVector instead of template class.
    git-svn-id: file:///scratch/cjstimp/svn/cubit/CUBIT_SOURCE/trunk@409549 d353b8dc-a10e-11dd-9ca7-d543032e456e

--- V_TetMetric.hpp
+++ V_TetMetric.hpp
@@ -32,6 +32,7 @@ namespace verdict {
 //  want to avoid direct dependence of verdict on STK for now, this is the one function it really needs so just
 //  copy it here for now.
 //
+  /*
     inline double stk_sqrt(double x) {
       return std::sqrt(x);
     }
@@ -39,7 +40,7 @@ namespace verdict {
     inline T stk_sqrt(T x) {
       return stk::math::sqrt(x);
     }
-
+    */
 
 
 
@@ -62,47 +63,29 @@ namespace verdict {
 
 
 
-template <typename T>
-T tet_dimension( int num_nodes, const T* coordinates ) {
+double tet_dimension( int num_nodes, double coordinates[][3] ) {
 
-  const T* c0 = coordinates;
-  const T* c1 = coordinates+3;
-  const T* c2 = coordinates+6;
-  const T* c3 = coordinates+9;
-
-  T centroid[3];
-  centroid[0] = 0.25*(c0[0] + c1[0] + c2[0] + c3[0]);
-  centroid[1] = 0.25*(c0[1] + c1[1] + c2[1] + c3[1]);
-  centroid[2] = 0.25*(c0[2] + c1[2] + c2[2] + c3[2]);
+  VerdictVector c0( coordinates[0][0], coordinates[0][1], coordinates[0][2] ); 
+  VerdictVector c1( coordinates[1][0], coordinates[1][1], coordinates[1][2] ); 
+  VerdictVector c2( coordinates[2][0], coordinates[2][1], coordinates[2][2] ); 
+  VerdictVector c3( coordinates[3][0], coordinates[3][1], coordinates[3][2] );     
+    
+  VerdictVector centroid = 0.25 * (c0 + c1 + c2 + c3);
+    
   //
   //  Most simplified calculation, distance from centroid to each face should be identical, so just calculate one
   //
-  T edge0[3];
-  edge0[0] = c1[0]-c0[0];
-  edge0[1] = c1[1]-c0[1];
-  edge0[2] = c1[2]-c0[2];
 
-  T edge1[3];
-  edge1[0] = c2[0]-c0[0];
-  edge1[1] = c2[1]-c0[1];
-  edge1[2] = c2[2]-c0[2];
+  VerdictVector edge0 = c1-c0;
+  
+  VerdictVector edge1 = c2-c0;
+                       
+  VerdictVector normal = edge0*edge1;    
+  normal.normalize();
 
-  T normal[3];
-  normal[0] = edge0[1]*edge1[2] - edge0[2]*edge1[1];
-  normal[1] = edge0[2]*edge1[0] - edge0[0]*edge1[2];
-  normal[2] = edge0[0]*edge1[1] - edge0[1]*edge1[0];
+  VerdictVector p = centroid - c0;
 
-  T normalLen = stk_sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2]);
-  normal[0] /= normalLen;
-  normal[1] /= normalLen;
-  normal[2] /= normalLen;
-
-  T p[3];
-  p[0] = centroid[0]-c0[0];
-  p[1] = centroid[1]-c0[1]; 
-  p[2] = centroid[2]-c0[2];
-
-  return 2.0*(normal[0]*p[0] + normal[1]*p[1] + normal[2]*p[2]);
+  return 2.0*normal%p;   
 }
 
 
