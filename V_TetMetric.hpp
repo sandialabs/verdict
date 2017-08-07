9cecd8378b36f8f867752b288f5ee13cc5a9661c
Corey Ernst <cdernst@sandia.gov>
2017-08-07 16:09:39 +0000

    Changing tet_dimension metric (inradius of sphere) to be:
    3*tet_volume / (a1+a2+a3+a4)
    
    where a1 is the area of tri 1, a2 is the area of tri 2, ...
    
    git-svn-id: file:///scratch/cjstimp/svn/cubit/CUBIT_SOURCE/trunk@409670 d353b8dc-a10e-11dd-9ca7-d543032e456e

--- V_TetMetric.hpp
+++ V_TetMetric.hpp
@@ -23,87 +23,67 @@
 
 #include <assert.h>
 #include <math.h>
+#include "verdict.h"
 #include "VerdictVector.hpp"
 
 
 namespace verdict {
 
-//
-//  want to avoid direct dependence of verdict on STK for now, this is the one function it really needs so just
-//  copy it here for now.
-//
-  /*
-    inline double stk_sqrt(double x) {
-      return std::sqrt(x);
-    }
-    template <typename T>
-    inline T stk_sqrt(T x) {
-      return stk::math::sqrt(x);
-    }
-    */
 
-
-
-
-
-
-
-
-
-  //
-  //  Compute the characteristic length scale of an element, this specifically influnces the explicit time step on the element
-  //    edge0 = c1-c0;
-  //    edge1 = c2-c0;
-  //    normal = Cross(edge0, edge1)  # Note, not a unit normal
-  //    p = centroid-c0
-  //    normal^ = UnitVector(normal)
-  //    dist = Dot(p, normal)
-  //    
-  //
-
-
-
-double tet_dimension( int num_nodes, double coordinates[][3] ) {
- 
-    VerdictVector c0(coordinates[0][0], coordinates[0][1], coordinates[0][2]);
-    VerdictVector c1(coordinates[1][0], coordinates[1][1], coordinates[1][2]);
-    VerdictVector c2(coordinates[2][0], coordinates[2][1], coordinates[2][2]);
-    VerdictVector c3(coordinates[3][0], coordinates[3][1], coordinates[3][2]);
-
-    VerdictVector centroid = 0.25 * (c0 + c1 + c2 + c3);    
+  double tet_dimension( int num_nodes, double coordinates[][3] ) { 
     
-    VerdictVector edge0 = c1 - c0;
-    VerdictVector edge1 = c2 - c0;
-    VerdictVector normal = edge0*edge1;
-    normal.normalize();
-    VerdictVector p = centroid - c0;
-    double radius0 = normal%p; 
+    //area1 (0,1,2)
+    double a1 = verdict::tri_area(3, coordinates);
+    
+    //area2 (0,3,1)
+    double tmp_coords[3][3];
+    tmp_coords[0][0] = coordinates[0][0];
+    tmp_coords[0][1] = coordinates[0][1];
+    tmp_coords[0][2] = coordinates[0][2];
 
-    edge0 = c3 - c0;
-    edge1 = c1 - c0;
-    normal = edge0*edge1;
-    normal.normalize();
-    p = centroid - c0;    
-    double radius1 = normal%p; 
+    tmp_coords[1][0] = coordinates[3][0];
+    tmp_coords[1][1] = coordinates[3][1];
+    tmp_coords[1][2] = coordinates[3][2];
 
-    edge0 = c2 - c0;
-    edge1 = c3 - c0;
-    normal = edge0*edge1;
-    normal.normalize();
-    p = centroid - c0;    
-    double radius2 = normal%p; 
+    tmp_coords[2][0] = coordinates[1][0];
+    tmp_coords[2][1] = coordinates[1][1];
+    tmp_coords[2][2] = coordinates[1][2];
 
-    edge0 = c3 - c1;
-    edge1 = c2 - c1;
-    normal = edge0*edge1;
-    normal.normalize();
-    p = centroid - c1;
-    double radius3 = normal%p; 
+    double a2 = verdict::tri_area(3, tmp_coords);
 
-    return 0.5*(radius0 + radius1 + radius2 + radius3 );
+    //area3 (0,2,3)
+    tmp_coords[1][0] = coordinates[2][0];
+    tmp_coords[1][1] = coordinates[2][1];
+    tmp_coords[1][2] = coordinates[2][2];
+
+    tmp_coords[2][0] = coordinates[3][0];
+    tmp_coords[2][1] = coordinates[3][1];
+    tmp_coords[2][2] = coordinates[3][2];
+    
+    double a3 = verdict::tri_area(3, tmp_coords);
+
+    //area4 (1,3,2)
+    tmp_coords[0][0] = coordinates[1][0];
+    tmp_coords[0][1] = coordinates[1][1];
+    tmp_coords[0][2] = coordinates[1][2];
+
+    tmp_coords[1][0] = coordinates[3][0];
+    tmp_coords[1][1] = coordinates[3][1];
+    tmp_coords[1][2] = coordinates[3][2];
+
+    tmp_coords[2][0] = coordinates[2][0];
+    tmp_coords[2][1] = coordinates[2][1];
+    tmp_coords[2][2] = coordinates[2][2];
+    
+    double a4 = verdict::tri_area(3, tmp_coords);
+
+    double tet_volume = verdict::tet_volume(4, coordinates);
+
+    double inradius = 3*tet_volume/(a1 + a2 + a3 + a4);
+
+    return inradius;
 }
-
-
+  
 
 
 }
